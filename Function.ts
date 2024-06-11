bcffcc28a7c2de541291cf3ab3342fc6c5bee1a5
Sye-5pace <owusilasyp00@gmail.com>
2024-06-11 17:14:24 +0100

    create captalize function

--- Function.ts
+++ Function.ts
@@ -1,2 +1,10 @@
 //Task #1
 // a. captalize(str) 
+const captalize = (str) => {
+    if( typeof str !== string){
+        throw new TypeError ('Not a string')
+    }
+
+    return !str ? str : str.charAt(0).toUpperCase() + str.slice(1);
+}
+
