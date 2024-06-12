9c9bff32f468b5aa72e005fc9be563b7d5a28c9b
Sye-5pace <owusilasyp00@gmail.com>
2024-06-12 12:19:38 +0100

    update task 1 & 2 ,create sumArr and avgArr function

--- Function.ts
+++ Function.ts
@@ -1,42 +1,43 @@
 //Task #1 String Transformation
+const strValid = (str: string) => {
+  if (typeof str !== 'string') {
+    throw new TypeError('Not a string');
+  }
+}
 // a. captalize(str) 
-const captalize = (str) => {
-    if( typeof str !== string){
-        throw new TypeError ('Not a string')
-    }
-
-    return !str ? str : str.charAt(0).toUpperCase() + str.slice(1);
+const capitalize = (str: string) => {
+  strValid(str)
+  return !str ? str : str.charAt(0).toUpperCase() + str.slice(1);
 }
-
-// d. wordCount(str) 
-const wordCount = (str) => {
-    if (typeof str !== 'string') {
-      throw new TypeError('Not a string');
-    }
-    const words = str.trim().split(/\s+/);
-    return words.length;
+  
+  // d. wordCount(str) 
+const wordCount = (str: string) => {
+  strValid(str)
+  const words = str.trim().split(/\s+/);
+  return words.length;
 }
-
-//b. reverse(str)
-const reverse = (str) => {
-    if (typeof str !== 'string') {
-      throw new TypeError('Not a string');
-    }
-    return str.split('').reverse().join('');
+  
+  //b. reverse(str)
+const reverse = (str: string) => {
+  strValid(str)
+  let results: string = ''
+  for( i = str.length -1; i >=0; i--){
+    results += str[i];
+  }
+  return results
 }
-
-// c. isPalindrome(str) 
+  
+  // c. isPalindrome(str) 
 const isPalindrome = (str) => {
-    if (typeof str !== 'string') {
-      throw new TypeError('Input must be a string');
-    }
-    const cleanStr = str.replace(/\W/g, '').toLowerCase();
-    return cleanStr === reverse(cleanStr);
+  strValid(str)
+  const cleanStr = str.replace(/\W/g, '').toLowerCase();
+  return cleanStr === reverse(cleanStr);
 }
+  
 
 
 // Task #2 Array transformations
-const arrValid = (arr) => {
+const arrValid = (arr: number[]) => {
     if(!Array.isArray(arr)){
         throw new TypeError('Input must be an array')
     }
@@ -44,18 +45,40 @@ const arrValid = (arr) => {
 
 
 // b. filterEven (arr)
-const filterEven = (arr) = > {
-  arrValid(arr)   
-  return arr.filter( num => num % 2 !== 0)
+const filterEven = (arr: number[]) => {
+  arrValid(arr)   ,
+  return arr.filter( num => num % 2 === 0);
 }
 
 // a. doubleArr (arr)
-const doubleArr = (arr) => {
+const doubleArr = (arr: number[]) => {
   arrValid(arr)
   return arr.map( num => num * 2 );
 }
 
 // c. sumArr
-const sumArr = (arr) => {
-    arrValid(arr)
+const sumArr = (arr: number[]) => {
+    arrValid(arr);
+    return arr.reduce((acc, num) => acc + num, 0); 
 }
+
+// d. avg(arr)
+const avgArr = (arr: number[]) => {
+  arrValid(arr);
+  if (arr.length === 0) {
+    return 0;
+  }
+  const total = sumArr(arr);
+  return total / arr.length;
+}
+
+
+
+  
+  
+  
+ 
+  
+ 
+
+
